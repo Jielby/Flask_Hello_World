16f1ac03bb5c68f0d76b5b75e3d09f073f3cda4d
Jielby <a.gosse@ecoles-epsi.net>
2025-03-31 12:22:06 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,21 +1,16 @@
 from flask import Flask
 from flask import render_template
-from flask import json
-import sqlite3
-                                                                                                                                       
-app = Flask(__name__)
+from flask import json                                                                                                                                     
+app = Flask(__name__)                                                                                                                  
 
-@app.route("/contact/")
-def MaPremiereAPI():
-    return render_template("contact.html")
-                                                                                                                                       
-@app.route('/')
-def hello_world():
-    return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>" #Comm2
 
-@app.route('/exercices/')
-def exercices():
-    return render_template('exercices.html')
-                                                                                                               
+@app.route('/<int:valeur>')
+def exercice(valeur):
+    etoiles = ''
+    for i in range(valeur):
+        etoiles += '*'
+    return etoiles #comm
+
+
 if __name__ == "__main__":
   app.run(debug=True)
