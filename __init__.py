77685292dcc216a8f91c2c6c0c1291b718c68357
Jielby <a.gosse@ecoles-epsi.net>
2025-04-07 12:09:06 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,23 +1,22 @@
 from flask import Flask
-from flask import render_template
-from flask import json                                                                                                                                     
-app = Flask(__name__)                                                                                                                  
 
-@app.route('/<int:valeur>')
-def exercice(valeur):
-    a, b = 0, 1
-    result = []
+app = Flask(__name__)
 
-    result.append(a)
-    if valeur > 1:
-        result.append(b)
+@app.route('/somme/<int:n>')
+def somme(n):
+    total = 0
 
-    for i in range(2, valeur):
-        a, b = b, a + b
-        result.append(b)
-
-    return ', '.join(str(x) for x in result)
+    # Boucle pour parcourir les nombres de 1 à n
+    for i in range(1, n + 1):
+        # Vérification si divisible par 5 ou 7 mais pas par 11
+        if (i % 5 == 0 or i % 7 == 0) and i % 11 != 0:
+            total += i
         
+        # Si la somme dépasse 5000, on arrête la boucle
+        if total > 5000:
+            break
+    
+    return f"La somme finale est : {total}"
+
 if __name__ == "__main__":
     app.run(debug=True)
-
