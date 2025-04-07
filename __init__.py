ca63e8f4462a600958da24a23312d1c018adf321
Jielby <a.gosse@ecoles-epsi.net>
2025-04-07 09:41:30 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,6 +9,8 @@ def exercice(valeur):
     etoiles = ''
     for j in range(valeur):
         for i in range(valeur-j):
+            etoiles += '+'   
+        for k in range(j+1):
             etoiles += '*'
         etoiles += '<br>'
     return etoiles
