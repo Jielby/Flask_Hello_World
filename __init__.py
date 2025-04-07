cd14760031842866012ee8c7739163792420241d
Jielby <a.gosse@ecoles-epsi.net>
2025-04-07 10:21:58 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,13 +5,18 @@ app = Flask(__name__)
 
 
 @app.route('/<int:valeur>')
-def exercice(valeur):
-    for i in range(1, valeur + 1):
-        ligne = ' ' * (valeur - i)  # Ajoute les espaces au début
-        ligne += ''.join(str(j) for j in range(1, i + 1))  # Partie croissante
-        ligne += ''.join(str(j) for j in range(i - 1, 0, -1))  # Partie décroissante
-        print(ligne)
+def exercice():
+        n = 5
+        lignes = []
+    for i in range(1, n + 1):
+        ligne = ' ' * (n - i)
+        for j in range(1, i + 1):
+            ligne += str(j)
+        for j in range(i - 1, 0, -1):
+            ligne += str(j)
+        lignes.append(ligne)
 
+    return render_template('pyramide.html', lignes=lignes)
 
 if __name__ == "__main__":
   app.run(debug=True)
