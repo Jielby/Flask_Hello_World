79f3236bd147fdbd525092aef2ee31ce5a0817c8
Jielby <a.gosse@ecoles-epsi.net>
2025-04-07 11:04:13 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,15 +6,20 @@ app = Flask(__name__)
 @app.route('/<int:valeur>')
 def exercice(valeur):
     a, b = 0, 1
-    suite = [a, b]
+    result = []
 
-    for _ in range(2, n):
-    a, b = b, a + b
-    suite.append(b)
+    result.append(a)
+    if n > 1:
+        result.append(b)
 
-    return ', '.join(str(nombre) for nombre in suite)
+    # Calcul des autres termes
+    for i in range(2, n):
+        a, b = b, a + b
+        result.append(b)
 
-
-
+    # Affichage de la suite sous forme de texte
+    return ', '.join(str(x) for x in result)
+        
 if __name__ == "__main__":
     app.run(debug=True)
+
