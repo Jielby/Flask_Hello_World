ae6fe7c525f79f33679a62780bf42bcbac135090
Jielby <a.gosse@ecoles-epsi.net>
2025-04-07 10:31:26 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,14 +6,25 @@ app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
-    etoiles = ''
-    for j in range(valeur):
-        for i in range(valeur-j):
-            etoiles += '+'   
-        for k in range(j+1):
-            etoiles += '*'
-        etoiles += '<br>'
-    return etoiles
+# Demander à l'utilisateur d'entrer un nombre
+    n = int(input("Entrez la hauteur de la pyramide : "))
+
+# Boucle pour chaque ligne
+    for i in range(1, n + 1):
+    # Affiche les espaces
+        print(' ' * (n - i), end='')
+
+    # Affiche les nombres qui montent
+        for j in range(1, i + 1):
+            print(j, end='')
+
+    # Affiche les nombres qui descendent
+        for j in range(i - 1, 0, -1):
+            print(j, end='')
+
+    # Aller à la ligne suivante
+        print()
+
 
 if __name__ == "__main__":
   app.run(debug=True)
