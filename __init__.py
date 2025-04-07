984e2efb22202d4e37fe9a59d2721328c9df037f
Jielby <a.gosse@ecoles-epsi.net>
2025-04-07 12:43:38 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,15 +9,15 @@ def somme(n):
     # Boucle pour parcourir les nombres de 1 à n
     for i in range(1, n + 1):
         # Vérification si divisible par 5 ou 7, mais pas par 11
-        if (i % 5 == 0 or i % 7 == 0) and i % 11 != 0:
+        if i % 11 != 0:
+            continue
+        if (i % 5 == 0 or i % 7 == 0):
+            if total + i > 5000:
+                break
             total += i
-        
-        # Si la somme dépasse 5000, on arrête la boucle immédiatement
-        if total > 5000:
-            total = 5000
-            break
+            
     
-    return f"La somme finale est : {total}"
+    return f'<pre>La somme finale est : {total}</pre>"
 
 if __name__ == "__main__":
     app.run(debug=True)
