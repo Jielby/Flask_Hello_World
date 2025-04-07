c4bf4ba37983e7e9f44413a7a5e9d7a6555c3265
Jielby <a.gosse@ecoles-epsi.net>
2025-04-07 14:47:15 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,26 @@
-from flask import Flask
+from flask import Flask, abort
 
 app = Flask(__name__)
 
 @app.route('/<path:valeurs>')
 def exercice(valeurs):
-    liste_nombres = valeurs.split('/')
-    liste_nombres = [int(n) for n in liste_nombres]
-    resultat = 0
-    for n in liste_nombres:
-        resultat = resultat + n
-    return str(resultat)
+    try:
+        liste_nombres = [int(n) for n in valeurs.split('/')]
+
+        # Initialiser le maximum avec le premier élément
+        maximum = liste_nombres[0]
+        for n in liste_nombres:
+            if n > maximum:
+                maximum = n
+
+        return f"Le maximum est : {maximum}"
+
+    except (ValueError, IndexError):
+        abort(400, description="Assurez-vous de saisir uniquement des entiers dans l'URL.")
+
+@app.route('/')
+def accueil():
+    return "Utilisez une URL comme /10/25/7 pour trouver le nombre maximum."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
