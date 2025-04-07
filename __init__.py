8123b19c306f83ecdd54b746263c30de543515b2
Jielby <a.gosse@ecoles-epsi.net>
2025-04-07 12:32:31 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,10 +6,13 @@ app = Flask(__name__)
 def somme(n):
     total = 0
 
+    # Boucle pour parcourir les nombres de 1 à n
     for i in range(1, n + 1):
+        # Vérification si divisible par 5 ou 7, mais pas par 11
         if (i % 5 == 0 or i % 7 == 0) and i % 11 != 0:
             total += i
         
+        # Si la somme dépasse 5000, on arrête la boucle immédiatement
         if total > 5000:
             total = 5000
             break
