bb5b780e7fcf539f6f958e6fd4c8ec1c2738b012
Jielby <a.gosse@ecoles-epsi.net>
2025-04-07 12:45:42 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,13 +5,10 @@ app = Flask(__name__)
 @app.route('/<int:n>')
 def somme(n):
     total = 0
-
-    # Boucle pour parcourir les nombres de 1 à n
     for i in range(1, n + 1):
-        # Vérification si divisible par 5 ou 7, mais pas par 11
-        if i % 11 != 0:
+        if i % 11 == 0:
             continue
-        if (i % 5 == 0 or i % 7 == 0):
+        if i % 5 == 0 or i % 7 == 0:
             if total + i > 5000:
                 break
             total += i
